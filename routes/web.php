2cc2f1110d507644bb5877f22d712b9e8d146322
momenbh <mdmomenbhuiya@gmail.com>
2022-06-28 16:38:52 +0600

    veiw

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\ChatagoryController;
 
 
 /*
@@ -26,3 +27,9 @@ Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard')
 Route::get('/product/view',[ProductController::class,'view'])->name('view.product');
 Route::get('/product/from',[ProductController::class,'from'])->name('from.product');
 Route::post('/product/store',[ProductController::class,'store'])->name('store.product');
+
+
+// operation chetagory
+ Route::get('/chatagory/view',[ChatagoryController::class,'view'])->name('view.chatagory');
+ Route::get('/chatagory/from',[ChatagoryController::class,'from'])->name('from.chatagory');
+ Route::post('/chatagory/store',[ChatagoryController::class,'store'])->name('store.chatagory');
